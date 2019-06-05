7c3303c4de05e7f661c3b461750266d7f6f937cf
David Basil <info@basildavid.com>
2019-06-05 15:05:26 +0400

    add insert contact functionality

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/contacts', 'ContactController@create');
+
+Route::post('/contacts', 'ContactController@store')->name('contact.store');
